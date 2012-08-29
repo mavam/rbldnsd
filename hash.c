f347bae0a236d52791e552f6f163337588879794
Matthias Vallentin <vallentin@icir.org>
2012-08-29 14:55:10 -0700

    Adhere to given coding style.

--- hash.c
+++ hash.c
@@ -9,23 +9,23 @@
 
 int sha256(void *bytes, size_t size, char out[64]) {
 #ifndef NO_ANONYMIZE
-    unsigned char digest[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    if (! SHA256_Init(&sha256))
-        return 0;
-
-    if (! SHA256_Update(&sha256, bytes, size))
-        return 0;
-
-    if (! SHA256_Final(digest, &sha256))
-        return 0;
-
-    int i;
-    for (i = 0; i < SHA256_DIGEST_LENGTH; ++i)
-        sprintf(out + (i * 2), "%02x", digest[i]);
-
-    return 1;
-#else
+  unsigned char digest[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha256;
+  if (! SHA256_Init(&sha256))
     return 0;
+
+  if (! SHA256_Update(&sha256, bytes, size))
+    return 0;
+
+  if (! SHA256_Final(digest, &sha256))
+    return 0;
+
+  int i;
+  for (i = 0; i < SHA256_DIGEST_LENGTH; ++i)
+    sprintf(out + (i * 2), "%02x", digest[i]);
+
+  return 1;
+#else
+  return 0;
 #endif
 }
